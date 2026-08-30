e2aa22a69650b3dd5c1bac9addc285564a9e2146
Jianlong Liu <JianlongLiu@users.noreply.github.com>
2026-08-31 00:36:28 +0800

    zen: windows.lua 给 zen 加 opacity 0.92 0.88 毛玻璃透壁纸 (idle_inhibit 保持看视频不锁屏)

--- windows.lua
+++ windows.lua
@@ -8,4 +8,4 @@ o.window("org.gnome.Nautilus", { float = true, opacity = "0.88 0.82" })
 o.window("org.gnome.Loupe", { opacity = "0.88 0.82" })
 
 -- Don't lock the screen while Zen Browser is open (watching bilibili etc., windowed or fullscreen).
-o.window("zen", { idle_inhibit = "always" })
+o.window("zen", { idle_inhibit = "always", opacity = "0.92 0.88" })
